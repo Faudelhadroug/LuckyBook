7b8948e9b33d19c766179dc0081046fb9b4d062e
Faudel Hadroug <hadroug.faudel@gmail.com>
2021-12-04 17:49:24 +0100

    Init app

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <Navbar />
+            </nav>
+        </header>
+        <main>
+            <slot />
+        </main>
+        <footer>
+            <!-- Footer Content -->
+        </footer>
+    </div>
+</template>
