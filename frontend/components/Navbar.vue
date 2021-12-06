f670e6e25690fcac689ec73a9a4ba220d8354b98
Faudel Hadroug <hadroug.faudel@gmail.com>
2021-12-06 18:57:06 +0100

    Correction path tailwind for nuxt config and added Navbar (desktop and mobile)

---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template> 
+    <div>
+        <div class="bg-black text-white justify-around text-3xl fixed w-screen py-5 hidden md:flex">
+            <ul class="flex space-x-20">
+                <a href="#"><li>Home</li></a> 
+                <a href="#"><li>Mint</li></a> 
+                <a href="#"><li>Roadmap</li></a> 
+                <a href="#"><li>Rarity</li></a> 
+            </ul>
+            <p>Bookz</p>
+        </div>
+        <div class="md:hidden">
+            <div class="text-2xl bg-black text-white flex justify-around fixed w-screen py-5">
+                <p>Bookz</p>
+                <img @click="visibleMobileMenu = true" class="w-10" src="../assets/images/menuMobileIcon.svg" alt="Drop menu icon">
+            </div>
+            <div class="bg-black fixed w-full h-full overflow-hidden" :class="visibleMobileMenu ? 'block' : 'hidden'">
+                <img @click="visibleMobileMenu = false" class="absolute left-3/4 mt-10 w-10" src="../assets/images/crossIcon.svg" alt="Cross close menu Icon">
+                <div class="flex h-screen">
+                    <ul class="text-4xl text-center m-auto space-y-4 text-white">
+                        <a href="#"><li class="mb-10">Home</li></a> 
+                        <a href="#"><li class="mb-10">Mint</li></a> 
+                        <a href="#"><li class="mb-10">Roadmap</li></a> 
+                        <a href="#"><li>Rarity</li></a> 
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+
+export default {
+    setup () {
+        const state = reactive({
+            visibleMobileMenu: false,
+        })
+    
+        return {
+            ...toRefs(state),
+        }
+    }
+}
+</script>
