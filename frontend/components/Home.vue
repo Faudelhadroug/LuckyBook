b44e76a63c6a07e201ea48ea7190e6df8d13940d
Faudel Hadroug <hadroug.faudel@gmail.com>
2021-12-08 04:58:11 +0100

    updated display navbar, added image and home components

--- frontend/components/Home.vue
+++ frontend/components/Home.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        <h1>Welcome</h1>
+    <div>   
+        <div class="pt-20 bg-black"></div>
+        <div class="relative">
+            <button class="bg-blue-500 text-white text-sm rounded-full p-3 md:p-4 flex absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:text-xl lg:p-8 lg:text-4xl">
+                Follow us on <img class="w-5 ml-1 mt-0.5 lg:w-10 lg:ml-3" src="../assets/images/twitterIconWhite.svg" alt="Twitter icon white">
+            </button>
+            <img src="../assets/images/bookHome.svg" alt="Lucky book home page">
+        </div>
     </div>
 </template>
 
--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template> 
     <div>
-        <div class="bg-black text-white justify-around text-3xl fixed w-screen py-5 hidden md:flex">
+        <div class="bg-black text-white justify-around text-3xl fixed w-screen py-5 hidden z-30 md:flex">
             <p class="text-green-500">LuckyBook</p>
             <ul class="flex space-x-20">
                 <a href="#"><li>Mint</li></a> 
@@ -9,18 +9,18 @@
             </ul>
         </div>
         <div class="md:hidden">
-            <div class="text-2xl bg-black text-white flex justify-around fixed w-screen py-5">
+            <div class="text-2xl bg-black text-white flex justify-around fixed w-screen py-5 z-30">
                 <p>Bookz</p>
                 <img @click="visibleMobileMenu = true" class="w-10" src="../assets/images/menuMobileIcon.svg" alt="Drop menu icon">
             </div>
-            <div class="bg-black fixed w-full h-full overflow-hidden" :class="visibleMobileMenu ? 'block' : 'hidden'">
+            <div class="bg-black fixed w-full h-full overflow-hidden z-30" :class="visibleMobileMenu ? 'block' : 'hidden'">
                 <img @click="visibleMobileMenu = false" class="absolute left-3/4 mt-10 w-10" src="../assets/images/crossIcon.svg" alt="Cross close menu Icon">
                 <div class="flex h-screen">
                     <ul class="text-4xl text-center m-auto space-y-4 text-white">
-                        <a href="#"><li class="mb-10">Home</li></a> 
-                        <a href="#"><li class="mb-10">Mint</li></a> 
-                        <a href="#"><li class="mb-10">Roadmap</li></a> 
-                        <a href="#"><li>Rarity</li></a> 
+                        <a href="#"><li class="mb-10">Home</li></a>
+                        <a href="#"><li class="mb-10">Mint</li></a>
+                        <a href="#"><li class="mb-10">Roadmap</li></a>
+                        <a href="#"><li>Rarity</li></a>
                     </ul>
                 </div>
             </div>
