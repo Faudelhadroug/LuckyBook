6e8f9c58bd4a176f2f4d49f0f4e7b870897cb55a
Faudel Hadroug <hadroug.faudel@gmail.com>
2021-12-14 15:29:13 +0100

    smooth scroll

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,31 +1,31 @@
 <template> 
     <div>
         <div class="bg-primary text-white justify-around text-3xl fixed w-screen py-5 hidden z-30 md:flex">
-            <a :href="$route.path == '/' ? '#home' : '/'">
+            <NuxtLink :to="{ path: '/', hash: '#home'}">
                 <p class="text-green-500">LuckyBook</p>
-            </a>
+            </NuxtLink>
             <ul class="flex space-x-20">
-                <a href="#mint"><li>Mint</li></a> 
-                <a href="#roadmap"><li>Roadmap</li></a> 
+                <NuxtLink :to="{ path: '/', hash: '#mint'}"><li>Mint</li></NuxtLink> 
+                <NuxtLink :to="{ path: '/', hash: '#roadmap'}"><li>Roadmap</li></NuxtLink> 
                 <NuxtLink to="/rarity"><li>Rarity</li></NuxtLink> 
-                <a href="#FAQ"><li>FAQ</li></a> 
+                <NuxtLink :to="{ path: '/', hash: '#FAQ'}"><li>FAQ</li></NuxtLink> 
             </ul>
         </div>
         <div class="md:hidden">
             <div class="text-2xl bg-primary text-white flex justify-around fixed w-screen py-5 z-30">
-           <a :href="$route.path == '/' ? '#home' : '/'">
+           <NuxtLink :to="{ path: '/', hash: '#home'}">
                 <p class="text-green-500">LuckyBook</p>
-            </a>
+            </NuxtLink>
                 <img @click="visibleMobileMenu = true" class="w-10" src="../assets/images/menuMobileIcon.svg" alt="Drop menu icon">
             </div>
             <div class="bg-primary fixed w-full h-full overflow-hidden z-30" :class="visibleMobileMenu ? 'block' : 'hidden'">
                 <img @click="visibleMobileMenu = false" class="absolute left-3/4 mt-10 w-10" src="../assets/images/crossIcon.svg" alt="Cross close menu Icon">
                 <div class="flex h-screen">
                     <ul class="text-4xl text-center m-auto space-y-4 text-white">
-                        <a @click="visibleMobileMenu = false" href="#mint"><li class="mb-10">Mint</li></a>
-                        <a @click="visibleMobileMenu = false" href="#roadmap"><li class="mb-10">Roadmap</li></a>
+                        <NuxtLink @click="visibleMobileMenu = false" :to="{ path: '/', hash: '#mint'}"><li class="mb-10">Mint</li></NuxtLink>
+                        <NuxtLink @click="visibleMobileMenu = false" :to="{ path: '/', hash: '#roadmap'}"><li class="mb-10">Roadmap</li></NuxtLink>
                         <NuxtLink @click="visibleMobileMenu = false" to="rarity"><li class="mb-10">Rarity</li></NuxtLink>
-                        <a @click="visibleMobileMenu = false" href="#FAQ"><li>FAQ</li></a> 
+                        <NuxtLink @click="visibleMobileMenu = false" :to="{ path: '/', hash: '#FAQ'}"><li>FAQ</li></NuxtLink> 
                     </ul>
                 </div>0
             </div>
