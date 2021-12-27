ae9d6dff304be36cd06ba597cfca992901c09034
Faudel Hadroug <hadroug.faudel@gmail.com>
2021-12-27 05:43:12 +0100

    try add 404

--- frontend/layouts/erreur.vue
+++ frontend/layouts/erreur.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-
+    <div class="flex flex-col min-h-screen">
+         <client-only>
+            <header>
+                <nav>
+                    <Navbar />
+                </nav>
+            </header>
+            <main class="flex-grow">
+              <div>
+                <h1 class="texter-center text-3xl md:text-5xl">404</h1>
+                <br>
+                <Nuxt-Link :to="{ path: '/', hash: '#home'}">
+                  <p class="text-center text-lg md:text-2xl underline">Go back home</p>
+                </Nuxt-Link>
+              </div>
+            </main>
+            <footer>
+                <Footbar />
+            </footer>
+         </client-only>
     </div>
 </template>
 
